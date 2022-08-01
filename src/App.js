93a92954bcbc79f6393cc687d056579f4d31368e
amine aqebli <aqebli36@gmail.com>
2022-08-01 02:10:10 +0100

    ExpenseItem Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ExpenseItem from './components/ExpenseItem'
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h2>Let's get started!1</h2>
+      <p>This is Also Visible!!</p>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
