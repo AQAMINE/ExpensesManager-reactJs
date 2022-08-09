bf39d157c2e6f27a96656ce4bb7e31d650bdd88c
amine aqebli <aqebli36@gmail.com>
2022-08-10 00:08:06 +0100

    Start Use State

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,22 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../Ui/Card';
+import { useState } from 'react';
+
 const ExpenseItem = (props) => {
-    
+
+    const [title, setTitle] = useState(props.title);
+    const clickHandler = () => {
+        setTitle('Amine');
+    }
     return (
         <Card className='expense-item '>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <button onClick={() => console.log('Wotking')}>Change Title</button>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
