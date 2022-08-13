e322b8ca5a9a5e2a58f07df7b5c29e4a93e0b196
amine aqebli <aqebli36@gmail.com>
2022-08-13 03:01:29 +0100

    Add Chart Without Config

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,18 @@
+import "./Chart.css";
+import ChartBar from "./ChartBar";
+
+const Chart = (props) => {
+    return (
+        <div className="chart">
+            {props.dataPoints.map((dataPoint) => (
+                <ChartBar
+                    key={dataPoint.label}
+                    value={dataPoint.value}
+                    maxValue={null}
+                    lable={dataPoint.label}
+                />
+            ))}
+        </div>
+    );
+};
+export default Chart;
